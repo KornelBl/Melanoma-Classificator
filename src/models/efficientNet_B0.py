8a056aa318e48452469fcdf82d3383fea28421e6
KornelBl <kornelblakala@wp.pl>
2020-10-17 00:23:49 +0200

    first model and Dataset generator

---
+++ src/models/efficientNet_B0.py
@@ -0,0 +1,14 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+
+
+def EffNet0(config, trainable_base=False) -> keras.models.Sequential:
+    conv_base = keras.applications.efficientnet.EfficientNetB0(weights='imagenet', include_top=False,
+                                                               input_shape=(256,256,3))
+    model = keras.models.Sequential()
+    model.add(conv_base)
+    model.add(keras.layers.GlobalMaxPooling2D(name="gap"))
+    model.add(keras.layers.Dense(1, activation="sigmoid", name="fc_out"))
+    conv_base.trainable = trainable_base
+    return model
+
