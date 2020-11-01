098f966156d9ffa2cccd07e45680dbdc01c1fc34
KornelBl <kornelblakala@wp.pl>
2020-11-01 21:14:23 +0100

    functional API in EffNet0 function

--- src/models/efficientNet_B0.py
+++ src/models/efficientNet_B0.py
@@ -1,19 +1,18 @@
 import tensorflow.keras as keras
 import tensorflow as tf
 
-
-def EffNet0(config, trainable_base=False) -> keras.models.Sequential:
+def EffNet0(config, trainable_base=False) -> keras.models.Model:
     output_bias = keras.initializers.Constant(config['output_bias'])
 
+    input_img = keras.layers.Input(config["input_shape"])
     conv_base = keras.applications.efficientnet.EfficientNetB0(weights='imagenet', include_top=False,
-                                                               input_shape=config["input_shape"])
-    model = keras.models.Sequential()
-    model.add(conv_base)
-    model.add(keras.layers.GlobalMaxPooling2D(name="gap"))
-    model.add(keras.layers.Dense(1024, activation='relu'))
-    model.add(keras.layers.Dropout(0.4))
-    model.add(keras.layers.Dense(1, activation="sigmoid", name="fc_out", bias_initializer=output_bias))
-    conv_base.trainable = trainable_base
+                                                               input_shape=config["input_shape"])(input_img)
+    conv_base.trainable = config["trainable_base"]
+    gmp = keras.layers.GlobalMaxPooling2D(name="gap")(conv_base)
+    dense1 = keras.layers.Dense(1024, activation='relu')(gmp)
+    dropout1 = keras.layers.Dropout(0.4)(dense1)
+    fc_out = keras.layers.Dense(1, activation="sigmoid", name="fc_out", bias_initializer=output_bias)(dropout1)
+    model = keras.models.Model(inputs=input_img, outputs=fc_out)
     return model
 
 
