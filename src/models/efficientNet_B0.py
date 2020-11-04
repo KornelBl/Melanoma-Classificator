712e94eaba34aa3f37ef55ef61e1b2c7ec091fe8
KornelBl <kornelblakala@wp.pl>
2020-11-04 12:13:58 +0100

    layer freezing tests

--- src/models/efficientNet_B0.py
+++ src/models/efficientNet_B0.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 def EffNet0(config, trainable_base=False) -> keras.models.Model:
     output_bias = keras.initializers.Constant(config['output_bias'])
     pd_dropout2 = None
+    pd_input = None
 
     if config["use_patient_data"]:
         pd_input = keras.layers.Input((9,))
@@ -20,6 +21,7 @@ def EffNet0(config, trainable_base=False) -> keras.models.Model:
     input_img = keras.layers.Input(config["input_shape"])
     conv_base = keras.applications.efficientnet.EfficientNetB0(weights='imagenet', include_top=False,
                                                                input_shape=config["input_shape"])(input_img)
+
     gmp = keras.layers.GlobalMaxPooling2D(name="gap")(conv_base)
     concat = keras.layers.concatenate([pd_dropout2, gmp])
     dense1 = keras.layers.Dense(1024)(concat)
@@ -29,14 +31,13 @@ def EffNet0(config, trainable_base=False) -> keras.models.Model:
     fc_out = keras.layers.Dense(1, activation="sigmoid", name="fc_out", bias_initializer=output_bias)(dropout1)
 
     model = keras.models.Model(inputs=[input_img, pd_input], outputs=fc_out)
-
-    model.layers[1].trainable = config["trainable_base"]
+    freeze_blocks(eff_net_model=model.layers[9],number_of_blocks=config["inner_blocks_frozen"])
+    #conv_base = unfreeze_blocks(conv_base, config["inner_blocks_unfrozen"])
     return model
 
 
-def unfreeze_blocks(model: keras.models.Sequential, number_of_blocks) -> keras.models.Sequential:
-    model_base = model.layers[0]
-    if number_of_blocks == "all":
+def unfreeze_blocks(model_base, number_of_blocks) -> keras.models.Sequential:
+    if number_of_blocks == 7:
         model_base.trainable = True
         return model_base
     numbers = [i for i in range(7, 7-number_of_blocks, -1)]
@@ -47,9 +48,25 @@ def unfreeze_blocks(model: keras.models.Sequential, number_of_blocks) -> keras.m
         block_names.append("top_")
     for layer in model_base.layers:
         for block_name in block_names:
-            print(f"{block_name}, {layer.name}")
             if block_name in layer.name:
-                print(f"IT IS ON")
                 layer.trainable = True
                 break
     return model_base
+
+
+def freeze_blocks(eff_net_model, number_of_blocks) -> keras.models.Model():
+    if number_of_blocks == 0:
+        return eff_net_model
+    block_names = []
+    for number in range(0, number_of_blocks+1):
+        block_names.append(f"block{number}")
+    block_names.append("stem_")
+    eff_net_model.layers[0].trainable = False
+    eff_net_model.layers[1].trainable = False
+    eff_net_model.layers[2].trainable = False
+    for layer in eff_net_model.layers:
+        for block_name in block_names:
+            if block_name in layer.name:
+                layer.trainable = False
+                break
+    return eff_net_model
