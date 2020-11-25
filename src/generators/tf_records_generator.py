d59621cf007826cf26abeb0d5e7374cbce80a77c
KornelBl <kornelblakala@wp.pl>
2020-11-25 21:50:27 +0100

    upsampling augmentation

--- src/generators/tf_records_generator.py
+++ src/generators/tf_records_generator.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 import re
 import numpy as np
+import math
 import PIL
 
 AUTO = tf.data.experimental.AUTOTUNE
@@ -23,18 +24,12 @@ def show_dataset(thumb_size, cols, rows, ds):
 
 
 def get_dataset(files, config, augment=False, shuffle=False, repeat=False,
-                labeled=True, return_image_names=True, patient_info=False):
+                labeled=True, return_image_names=True, upsampling=False, patient_info=False):
     ds = tf.data.TFRecordDataset(files, num_parallel_reads=AUTO)
     # ds = ds.cache()
 
     if repeat:
         ds = ds.repeat()
-
-    if shuffle:
-        ds = ds.shuffle(1024 * 8)
-        opt = tf.data.Options()
-        opt.experimental_deterministic = False
-        ds = ds.with_options(opt)
 
     if patient_info:
         ds = ds.map(read_labeled_tfrecord_with_patient_info, num_parallel_calls=AUTO)
@@ -44,9 +39,13 @@ def get_dataset(files, config, augment=False, shuffle=False, repeat=False,
         ds = ds.map(map_function, num_parallel_calls=AUTO)
 
 
-    if config['upsampling'] == True:
+    if upsampling == True:
         ds = ds.flat_map(upsample_map)
 
+    if augment == True:
+        ds = ds.map(augmentation_map,num_parallel_calls=AUTO)
+
+    ds = ds.shuffle(1024 * 8, seed=42)
 
 
     ds = ds.batch(config["batch_size"] * config["replicas"], drop_remainder=True)
@@ -54,13 +53,26 @@ def get_dataset(files, config, augment=False, shuffle=False, repeat=False,
     return ds
 
 
-def upsample_map(x):
-    tf.data.Dataset.from_tensors(x).repeat(num_of_upsamples(x) * 5)
+def augmentation_map(input_data, target):
+    img = input_data[0]
+    img = transform(img)
+    img = tf.image.random_flip_left_right(img)
+    img = tf.image.random_hue(img, 0.01)
+    img = tf.image.random_saturation(img, 0.7, 1.3)
+    img = tf.image.random_contrast(img, 0.8, 1.2)
+    img = tf.image.random_brightness(img, 0.1)
+    return (img, input_data[1]), target
+
+
+def upsample_map(x,y):
+    return tf.data.Dataset.from_tensors((x,y)).repeat(num_of_upsamples((x,y)))
 
 
 def num_of_upsamples(example):
-    return example[1]
-
+    if example[1] == 1:
+        return tf.cast(5, tf.int64)
+    else:
+        return tf.cast(1, tf.int64)
 
 def map_function_with_patient_info(input_data, imgname_or_label):
     return (prepare_image(input_data[0]), prepare_patient_info(input_data[1])), tf.cast(imgname_or_label, tf.int32)
@@ -122,3 +134,78 @@ def count_data_items(filenames):
     n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1))
          for filename in filenames]
     return np.sum(n)
+
+
+def get_mat(rotation, shear, height_zoom, width_zoom, height_shift, width_shift):
+    # returns 3x3 transformmatrix which transforms indicies
+
+    # CONVERT DEGREES TO RADIANS
+    rotation = math.pi * rotation / 180.
+    shear = math.pi * shear / 180.
+
+    def get_3x3_mat(lst):
+        return tf.reshape(tf.concat([lst], axis=0), [3, 3])
+
+    # ROTATION MATRIX
+    c1 = tf.math.cos(rotation)
+    s1 = tf.math.sin(rotation)
+    one = tf.constant([1], dtype='float32')
+    zero = tf.constant([0], dtype='float32')
+
+    rotation_matrix = get_3x3_mat([c1, s1, zero,
+                                   -s1, c1, zero,
+                                   zero, zero, one])
+    # SHEAR MATRIX
+    c2 = tf.math.cos(shear)
+    s2 = tf.math.sin(shear)
+
+    shear_matrix = get_3x3_mat([one, s2, zero,
+                                zero, c2, zero,
+                                zero, zero, one])
+    # ZOOM MATRIX
+    zoom_matrix = get_3x3_mat([one / height_zoom, zero, zero,
+                               zero, one / width_zoom, zero,
+                               zero, zero, one])
+    # SHIFT MATRIX
+    shift_matrix = get_3x3_mat([one, zero, height_shift,
+                                zero, one, width_shift,
+                                zero, zero, one])
+
+    return K.dot(K.dot(rotation_matrix, shear_matrix),
+                 K.dot(zoom_matrix, shift_matrix))
+
+
+def transform(image):
+    # input image - is one image of size [dim,dim,3] not a batch of [b,dim,dim,3]
+    # output - image randomly rotated, sheared, zoomed, and shifted
+    DIM = 256
+    XDIM = DIM % 2  # fix for size 331
+
+    rot = 180.0 * tf.random.normal([1], dtype='float32')
+    shr = 2.0 * tf.random.normal([1], dtype='float32')
+    h_zoom = 1.0 + tf.random.normal([1], dtype='float32') / 8.0
+    w_zoom = 1.0 + tf.random.normal([1], dtype='float32') / 8.0
+    h_shift = 8.0 * tf.random.normal([1], dtype='float32')
+    w_shift = 8.0 * tf.random.normal([1], dtype='float32')
+
+    # GET TRANSFORMATION MATRIX
+    m = get_mat(rot, shr, h_zoom, w_zoom, h_shift, w_shift)
+
+    # LIST DESTINATION PIXEL INDICES
+    x = tf.repeat(tf.range(DIM // 2, -DIM // 2, -1), DIM)
+    y = tf.tile(tf.range(-DIM // 2, DIM // 2), [DIM])
+    z = tf.ones([DIM * DIM], dtype='int32')
+    idx = tf.stack([x, y, z])
+
+    # ROTATE DESTINATION PIXELS ONTO ORIGIN PIXELS
+    idx2 = K.dot(m, tf.cast(idx, dtype='float32'))
+    idx2 = K.cast(idx2, dtype='int32')
+    idx2 = K.clip(idx2, -DIM // 2 + XDIM + 1, DIM // 2)
+
+    # FIND ORIGIN PIXEL VALUES
+    idx3 = tf.stack([DIM // 2 - idx2[0,], DIM // 2 - 1 + idx2[1,]])
+    d = tf.gather_nd(image, tf.transpose(idx3))
+
+    img = tf.reshape(d, [DIM, DIM, 3])
+    img = tf.clip_by_value(img, 0, 1)
+    return img
